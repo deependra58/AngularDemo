a9d4ec7b41f5b65e858b16566d5e03b219827a53
deependra58 <karkideependra58@gmail.com>
2018-05-08 13:14:19 +0545

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import {RouteRoutingModule} from './route-routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouteRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
